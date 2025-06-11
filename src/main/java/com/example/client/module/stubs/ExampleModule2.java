70c3cea0f6a3a2063535524cca12db645b75cf6e
cub1n09 <cubanpinto@gmail.com>
2025-06-11 23:29:40 +0100

    Create ExampleModule2.java

---
+++ src/main/java/com/example/client/module/stubs/ExampleModule2.java
@@ -0,0 +1,14 @@
+package com.example.client.module.stubs;
+
+import com.example.client.module.Module;
+
+public class ExampleModule2 extends Module {
+    public ExampleModule2() {
+        super("ExampleModule2");
+    }
+
+    @Override
+    public void onUpdate() {
+        // Stub logic here
+    }
+}
