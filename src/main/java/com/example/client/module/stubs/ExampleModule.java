46383081ce44f44d2fa4cd9ea1c5632b7e021488
cub1n09 <cubanpinto@gmail.com>
2025-06-11 23:29:54 +0100

    Add files via upload

---
+++ src/main/java/com/example/client/module/stubs/ExampleModule.java
@@ -0,0 +1,14 @@
+package com.example.client.module.stubs;
+
+import com.example.client.module.Module;
+
+public class ExampleModule extends Module {
+    public ExampleModule() {
+        super("ExampleModule");
+    }
+
+    @Override
+    public void onUpdate() {
+        // Stub logic here
+    }
+}
